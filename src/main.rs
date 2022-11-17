edf9efa4f49ca92bd8df5ada058652e57f3668d4
Marco Pivetta <ocramius@gmail.com>
2022-11-17 21:55:32 +0100

    Corrected XML template so it is valid XML
    XML structures must start with `<?xml`, not with `\n`

--- src/main.rs
+++ src/main.rs
@@ -44,8 +44,7 @@ struct ConfirmOciPaymentParameters {
     cxml_order_request_token: String,
 }
 
-const ORDER_REQUEST_TEMPLATE: &str = r###"
-<?xml version="1.0" encoding="UTF-8"?>
+const ORDER_REQUEST_TEMPLATE: &str = r###"<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE cXML SYSTEM "http://xml.cxml.org/schemas/cXML/1.2.014/cXML.dtd">
 <cXML payloadID="%unique-id%" timestamp="%timestamp%">
     <Header>
